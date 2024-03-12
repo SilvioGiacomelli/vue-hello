d15c41340255506fb639e7156a90c5f4b041c2c9
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-03-12 09:20:14 +0100

    counter

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ createApp({
     return {
       saluto: 'Hello',
       immagine: './immagine/01.webp',
+      testo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae molestias iure repudiandae illum. Odit suscipit rerum, assumenda numquam obcaecati nostrum illo earum adipisci libero velit reprehenderit vitae iste quasi dolore?',
+      contatore:0,
+      controller: false,
     }
   },
+  methods:{
+    incrementoContatore(){
+      this.contatore++;
+      console.log(this.contatore);
+      return oDD = 'red'
+    },
+    cambioColore(){
+      this.controller= !this.controller;
+    },
+    oDD(){
+      return this.contatore % 2 === 0;
+    }
+  }
 }).mount('#app')
